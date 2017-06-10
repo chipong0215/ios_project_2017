4d80cb1ed04889348fcea5b8fe778e483eee50ed
RAYHO219 <raywnwa@gmail.com>
2017-06-11 01:53:10 +0800

    EditProfileUI

---
+++ ios_wanted_project/ios_wanted_project/EditProfileTableViewController.swift
@@ -0,0 +1,23 @@
+//
+//  EditProfileTableViewController.swift
+//  ios_wanted_project
+//
+//  Created by valerie's mac on 2017/6/11.
+//  Copyright © 2017年 Pong. All rights reserved.
+//
+
+import UIKit
+
+class EditProfileTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    }
